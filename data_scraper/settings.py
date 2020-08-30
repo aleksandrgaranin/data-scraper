5d241b94fd4fd43924b4b3b867ae725bcc0b2fb1
aleksandGG <aleksandrgaranin89@gmail.com>
2020-08-30 13:21:44 -0700

    adjustments

--- data_scraper/settings.py
+++ data_scraper/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'krq0tt4sp@s$ji6+v1d_s(l8@_(53e*xj2^iu+m5l+p6g&i89&'
+SECRET_KEY = os.environ.get('SECRET_KEY', None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
