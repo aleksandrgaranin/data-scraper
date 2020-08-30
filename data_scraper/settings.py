5f317203f56d79074a6199d0345a03a346528f96
aleksandGG <aleksandrgaranin89@gmail.com>
2020-08-30 14:24:38 -0700

    added filter to scrape, now scrape adding to db only not existing elements

--- data_scraper/settings.py
+++ data_scraper/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', None)
+SECRET_KEY = 'krq0tt4sp@s$ji6+v1d_s(l8@_(53e*xj2^iu+m5l+p6g&i89&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ag-tsa-scraper.herokuapp.com', '127.0.0.1', 'localhost']
 
